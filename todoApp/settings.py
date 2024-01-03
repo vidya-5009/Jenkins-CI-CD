afd132b258afbc8c92d1a21bf444243924239dcc
vidya-5009 <109674743+vidya-5009@users.noreply.github.com>
2024-01-03 23:21:13 +0530

    Update settings.py

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django_prometheus.middleware.PromethuesBeforeMiddleware',
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
